2c7e177f4028742123ff2216aef641af625f8cf2
Jacob Handy <43211690+jacobhandy3@users.noreply.github.com>
2020-03-07 16:48:22 -0600

    Update main.py
    imports seperate file with Neural Network code and defines dictionary of attacks corresponding to the labels of that dataset.

--- main.py
+++ main.py
@@ -1,40 +1,27 @@
-from IPython import embed
-import numpy as np 
-import pickle
-import re
-import csv
-import utils
-import random
-import json
-import tensorflow as tf
-from sklearn import datasets
-from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import MinMaxScaler
-from tensorflow.python.framework import ops
-import pandas as pd
-np.random.seed(1337)
+import NNcode
 
-#load csv with csv reader
-with open('DataSets\CIC-IDS-2017\Monday-WorkingHours.pcap_ISCX.csv', 'r') as file:
-    reader = csv.reader(file)
-    dataList = list(reader)
-#get length of the columns of csv
-colLen = len(dataList[0])   #79
-#get length of the rows of csv
-rowLen = len(dataList)
+#Dictionary of all the specific attacksCIC for CICIDS Dataset
+attacksCIC = {
+  "BENIGN": 0,
+  "FTP-Patator": 1,
+  "SSH-Patator": 2,
+  "DoS slowloris": 3,
+  "DoS Slowhttptest": 4,
+  "DoS Hulk": 5,
+  "DoS GoldenEye": 6,
+  "Heartbleed": 7,
+  "Web Attack Brute Force": 8,
+  "Web Attack XSS": 9,
+  "Web Attack Sql Injection": 10,
+  "Infiltration": 11,
+  "Bot": 12,
+  "PortScan": 13,
+  "DDoS": 14,
+}
 
+#Dictionary of all specific attacksCIC for 2nd dataset
+attacksCIC2nd = {
 
-#create 2-D list to hold csv values
-csvList = [[] for x in range(rowLen)]
-i = 1
-j = 0
-a = 0
-#filling the 2-D array
-while i < rowLen:
-    while j < colLen - 1:
-        csvList[a].append(float(dataList[i][j]))
-        j += 1
-    j = 0
-    a += 1
-    i += 1
+}
 
+NNcode.NNAnalysis("DataSets\CIC-IDS-2017", attacksCIC)
