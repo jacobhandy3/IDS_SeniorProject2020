feac200b0a9926e22c2f1ef4c3524cb372f1d8a4
Jacob Handy <43211690+jacobhandy3@users.noreply.github.com>
2020-03-07 16:51:56 -0600

    new code for NNcode.py
    current Neural Network code with past results as models. Updated code to act as a function to call upon taking a string for folder path and a dictionary of attacks

--- NNcode.py
+++ NNcode.py
@@ -1,30 +1,75 @@
 from numpy import loadtxt
+from keras.optimizers import adam
 from keras.models import Sequential
-from keras.layers import Dense
+from keras.layers import Input, Dense
+from sklearn.preprocessing import StandardScaler
+from sklearn.model_selection import train_test_split
+from sklearn.metrics import confusion_matrix
+import pandas as pd
+import numpy as np
+import tensorflow as tf
+import keras as k
+import glob as glob
+import os
+import matplotlib.pyplot as plt
 
-dataset = loadtxt('DataSets\CIC-IDS-2017\Tuesday-WorkingHours.pcap_ISCX (C).csv', delimiter=',')
-
-
-# split into input (X) and output (y) variables
-X = dataset[:,:78]
-y = dataset[:,78]
-
-
-# define the keras model
-model = Sequential()
-model.add(Dense(12, input_dim=78, activation='relu'))
-model.add(Dense(8, activation='relu'))
-model.add(Dense(1, activation='sigmoid'))
+'''CONVERT TO FUNCTION BY PASSING FOLDER PATH & DICTIONARY OF ATTACKS'''
 
 
-# compile the keras model
-model.compile(loss='binary_crossentropy', optimizer='adam', metrics=['accuracy'])
+'''ANALYSIS OF CICIDS DATASET WITH 8 CSVs '''
 
+def NNAnalysis(path, attacks):
+    #open dataset folder path
+    os.chdir(path)
+    #find files with glob
+    fileList = glob.glob("*.csv")
+    #create a temp list
+    dataList = []
+    #loop thorugh the files
+    for file in fileList:
+        #read each file as csv with pandas
+        data = pd.read_csv(file, header=0)
+        #append to temp list
+        dataList.append(data)
+    #concat vertically
+    dataset = pd.concat(dataList, axis=0)
+    #replace all labels with a numeral assignment
+    for key in attacks:
+        dataset = dataset.replace(to_replace=key, value=attacks[key])
+    #replace the Infinity values with NaNs
+    dataset = dataset.replace(to_replace=np.inf, value=np.nan)
+    #drop any row with a NaN
+    dataset = dataset.dropna(how="any")
+    # split into input (X) and output (y) variables
+    colNum = dataset.shape[1]   #79?
+    X = dataset.iloc[:,0:(colNum-2)]
+    y = dataset.iloc[:,(colNum-1)]
+    print("Now onto the ML code")
+    # define the keras model
+    model = Sequential()
+    model.add(Dense(39, input_dim=77, activation='relu'))
+    model.add(Dense(26, activation='relu'))
+    model.add(Dense(15, activation='relu'))
+    model.add(Dense(15, activation='softmax'))
 
-# fit the keras model on the dataset
-model.fit(X, y, epochs=150, batch_size=10)
-
-
-# evaluate the keras model
-_, accuracy = model.evaluate(X, y)
-print('Accuracy: %.2f' % (accuracy*100))
+    # compile the keras model
+    model.compile(optimizer = adam(learning_rate=0.0005, beta_1=0.9, beta_2=0.999, amsgrad=False),loss='sparse_categorical_crossentropy', metrics =['accuracy'])
+    bs = 25
+    # fit the keras model on the dataset
+    hist = model.fit(X, y, epochs=50, batch_size=bs, validation_split=0.35)
+    #summarize history for accuracy
+    plt.plot(hist.history['accuracy'])
+    plt.plot(hist.history['val_accuracy'])
+    plt.title('model accuracy')
+    plt.ylabel('accuracy')
+    plt.xlabel('epoch')
+    plt.legend(['train','test'], loc='upper left')
+    plt.show()
+    #summarize history for loss
+    plt.plot(hist.history['loss'])
+    plt.plot(hist.history['val_loss'])
+    plt.title('model loss')
+    plt.ylabel('loss')
+    plt.xlabel('epoch')
+    plt.legend(['train', 'test'], loc='upper left')
+    plt.show()
