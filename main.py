d5b015512d1317dbeac58d2caeff8f01aa39b248
Jacob Handy <43211690+jacobhandy3@users.noreply.github.com>
2020-03-25 09:51:26 -0500

    Cleaner Code
    Made the code cleaner by adding more functions to do data processing tasks. Now both data sets share the same ML function with differing parameters

--- main.py
+++ main.py
@@ -18,6 +18,8 @@ attacksCIC = {
   "PortScan": 13,
   "DDoS": 14,
 }
+#Rows to review CIC data and add to dictionary
+CICrows = [78]
 #Dictionary of all specific attacksCIC for 2nd dataset
 mappingUNSW = {
   #attacks
@@ -32,7 +34,12 @@ mappingUNSW = {
   "Generic": 8,
   "Worms": 9,
 }
-#Rows to review data and add to dictionary
-UNSWrows = [0,1,2,3,11,45]
-#NNcode.CICAnalysis(r"DataSets\CIC-IDS-2017", attacksCIC)
-NNcode.UNSWAnalysis(r"DataSets\UNSW-NB15", mappingUNSW, UNSWrows)
+#Columns to review UNSW data and add to dictionary
+UNSWcols = [0,1,2,3,11,45]
+
+NNcode.NNanalysis(path=r"DataSets\CIC-IDS-2017", header=0, indexCol=None, 
+                  mapped=attacksCIC, rowL=CICrows, Xmax=77, labelCol=78, attackNum=15)
+
+NNcode.NNanalysis(path=r"DataSets\UNSW-NB15", header=None, indexCol=None,
+                    mapped=mappingUNSW,rowL=UNSWcols, Xmax=44, labelCol=45, attackNum=10,
+                    dropFeats=[1,3], missReplacement=["Benign"],missCols=[47])
