33198e20ad683c8885609b55afcda4b643607d6c
AdrianE-Gonzalez <adrian.e.gonzalez01@utrgv.edu>
2020-04-24 10:45:29 -0500

    added paths to save tree graphs, added plot diagram, cleaned up code and added comments

--- main.py
+++ main.py
@@ -2,20 +2,20 @@ import NNcode
 import decisionTree
 import load_Dataset
 import numpy
-import pandas 
-
+import pandas as pd
+import matplotlib.pyplot as plt
 #Loads CIC dataset
 def Load_First_DataSet():
     path,header,indexCol,mapped,colL,Xmax,labelCol,attackNum=load_Dataset.firstDataset()
-    dataset = load_Dataset.loadDataset(path=path,header=header,indexCol=indexCol,labelCol=labelCol,mapped=mapped,colL=colL)
-    return dataset,Xmax,attackNum,labelCol
+    dataset,attackNames = load_Dataset.loadDataset(path=path,header=header,indexCol=indexCol,labelCol=labelCol,mapped=mapped,colL=colL)
+    return dataset,Xmax,attackNum,labelCol,attackNames
 
 #Loads UNSW-NB15 dataset
 def Load_SecondDataSet():
     path,header,indexCol,mapped,colL,Xmax,labelCol,attackNum,dropFeats,missReplacement,missCols=load_Dataset.secondDataset()
-    dataset = load_Dataset.loadDataset(path=path,header=header,indexCol=indexCol,labelCol=labelCol,mapped=mapped,colL=colL,
+    dataset,attackNames = load_Dataset.loadDataset(path=path,header=header,indexCol=indexCol,labelCol=labelCol,mapped=mapped,colL=colL,
                                             dropFeats=dropFeats,missReplacement=missReplacement,missCols=missCols)
-    return dataset,Xmax,attackNum,labelCol
+    return dataset,Xmax,attackNum,labelCol, attackNames
 
 #Runs the NN File
 def Run_NNcode(dataset,Xmax,attackNum,labelCol):
@@ -23,69 +23,58 @@ def Run_NNcode(dataset,Xmax,attackNum,labelCol):
     return X,model
 
 #Runs the Decision Tree File
-def Run_DecisionTree(dataset,Xmax,attackNum,labelCol):
-    #Splits Dataset
-    inputData,target = decisionTree.trainDataset(dataset=dataset,Xmax=Xmax,labelCol=labelCol)
-    #Splits Dataset into train and test sets
-    X, y,attackNum,Xmax,X_train,y_train,X_test,y_test = decisionTree.trainModels(dataset,Xmax=Xmax,labelCol=labelCol,attackNum=attackNum,inputData=inputData,target=target)
+def Run_DecisionTree(dataset,Xmax,attackNum,labelCol,attackNames):
+    attackNames = attackNames.unique()
 
-    #The Range determines the Tree Depth
-    for xp in range(2,10):
-        #Name is meant to Name the PNG file
-        name = 'tree'+str(xp)+'.png'
-        #Creates and Runs Models
-        decisionTree.CreateModel(cntr=xp,X=X,X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,name=name)
+    #Split and Train Data
+    X,y,X_train,y_train,X_test,y_test= decisionTree.trainModels(dataset,labelCol)
+
+    #plot gini vs entropy accuracies
+    max_depth = []
+    acc_gini = []
+    acc_entropy = []
+
+    #Create and Printing Models, Appends predicted accuracies based on Criterion, Appends MaxDepth
+    for MaxDepth in range(2,30):
+        path="/media/southpark86/AMG1/School/Spring 2020/Senior Project/DecionTreeResults/CICIDS Dataset/Criterion_Entropy/Splitter_Plus_treeDepth/Splitter_Best/"
+        Criterion='entropy'
+        name = 'CIC_Dataset_Features_Criterion_Entropy_Splitter_Random_TreeDepth_'+str(MaxDepth)+'.png'
+        name = path+name
+        #Creates Entropy Models and Prints MaxDepth < 10
+        pred_acc= decisionTree.CreateModel(MaxDepth=MaxDepth,Criterion=Criterion,X=X,
+                                                    X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,
+                                                    name=name, attackNames=attackNames)
+        acc_gini.append(pred_acc)
+
+        path="/media/southpark86/AMG1/School/Spring 2020/Senior Project/DecionTreeResults/CICIDS Dataset/Criterion_Gini/Splitter_Plus_treeDepth/Splitter_Best/"
+        Criterion='gini'
+        name = 'CIC_Dataset_Features_Criterion_Gini_Splitter_Random_TreeDepth_'+str(MaxDepth)+'.png'
+        name = path+name
+        #Creates Gini Models and Prints MaxDepth < 10
+        pred_acc= decisionTree.CreateModel(MaxDepth=MaxDepth,Criterion=Criterion,X=X,X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test,name=name, attackNames=attackNames)
+        
+        acc_entropy.append(pred_acc)
+        max_depth.append(MaxDepth)
         print()
+
+    #stores the both gini and entropy accuraccies into a dataframe to use to plot the accuracies
+    d = pd.DataFrame({'acc_gini':pd.Series(acc_gini), 
+            'acc_entropy':pd.Series(acc_entropy),
+            'max_depth':pd.Series(max_depth)})
+    # plots gini accuracies vs entropy accuracies graph, Saves plot diagram in Dataset Folder
+    fig = plt.figure()
+    plt.plot('max_depth','acc_gini', data=d, label='gini')
+    plt.plot('max_depth','acc_entropy', data=d, label='entropy')
+    plt.xlabel('max_depth')
+    plt.ylabel('accuracy')
+    plt.title('Gini vs Entropy')
+    plt.legend()
+    plt.show()
+    fig.savefig('gini_vs_entropy.png')
 
 
 
 #Loaded 1st Dataset
-dataset,Xmax,attackNum,labelCol=Load_First_DataSet()
+dataset,Xmax,attackNum,labelCol,attackNames=Load_First_DataSet()
 #Ran Decision Tree
-Run_DecisionTree(dataset=dataset,Xmax=Xmax,attackNum=attackNum,labelCol=labelCol)
-
-#I Used this to Check any errors I had
-def testCode(inputData,target,dataset):    
-    #print("Input Data: ")
-    #print(inputData)
-    #print("Target: ")
-    print(target)
-    attacks = target[:,].unique()
-    print(sorted(attacks))
-
-    #inputData = inputData.drop(dataset.columns[14],axis=1)
-    #inputData = inputData.drop(dataset.columns[15],axis=1)
-
-    print(inputData.info())
-    print(dataset.info())
-'''
-print("PRINTING MODEL 2")
-cnt = 31
-for x in range(cnt):
-    rowNum =X.iloc[[x]] 
-    print("Target: ",target.iloc[[x]])
-    decisionTree.predictM(rowNum=rowNum, model=model)
-print("PRINTING MODEL 4")
-for x in range(cnt):
-    rowNum =X.iloc[[x]] 
-    print("Target: ",target.iloc[[x]])
-    decisionTree.predictM(rowNum=rowNum, model=model4)
-print("PRINTING MODEL 6")
-for x in range(cnt):
-    rowNum =X.iloc[[x]] 
-    print("Target: ",target.iloc[[x]])
-    decisionTree.predictM(rowNum=rowNum, model=model6)
-print("PRINTING MODEL 8")
-for x in range(cnt):
-    rowNum =X.iloc[[x]] 
-    print("Target: ",target.iloc[[x]])
-    decisionTree.predictM(rowNum=rowNum, model=model8)
-'''
-'''
-X,model = decisionTree.fineTune(X=X, model=model,attackNum=attackNum,Xmax=Xmax,X_train=X_train,y_train=y_train,X_test=X_test,y_test=y_test)
-
-for x in range(21):
-    rowNum =X.iloc[[x]] 
-    print("Target: ",target.iloc[[x]])
-    decisionTree.predictM(rowNum=rowNum, model=model)
-'''
+Run_DecisionTree(dataset=dataset,Xmax=Xmax,attackNum=attackNum,labelCol=labelCol,attackNames=attackNames)
